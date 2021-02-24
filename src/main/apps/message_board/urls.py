27c3418c51200867ad45ecd2f41b6586ef76e80b
ntwmac-dev <ntwmac.dev@gmail.com>
2021-02-24 16:48:02 +0000

    Implemented: create post (with dyno URL) and my_posts (indie users)

--- src/main/apps/message_board/urls.py
+++ src/main/apps/message_board/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
     Message_Board,
-    User_Post_Detail
+    Create_Post,
+    User_Post_Detail,
+    My_Posts
 )
 
 urlpatterns = [
-    path("",                Message_Board.as_view(),    name="message_board"),
-    path('<int:pk>/',       User_Post_Detail.as_view(), name='user_post'    ),
+    path('',                Message_Board.as_view(),    name='message_board'    ),
+    path('create/',         Create_Post.as_view(),      name='user_create_post' ),
+    path('<int:pk>/',       User_Post_Detail.as_view(), name='user_post'        ),
+    path('my_posts/',       My_Posts.as_view(),         name='my_posts'         ),
 ]
--- src/main/apps/message_board/views.py
+++ src/main/apps/message_board/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts           import render
+from django.urls                import reverse
 from .models                    import User_Post
 from django.views.generic import (
     ListView,
@@ -9,11 +10,33 @@ from django.views.generic import (
 )
 #   Views
 
+class Create_Post(CreateView):
+    model           = User_Post
+    fields          = ['subject', 'content']
+    template_name   = "message_board/user_create_post.html"
+    post_id = None
+
+    def form_valid(self, form):
+        form.instance.op_user = self.request.user
+        post            =   form.save()
+        self.post_id    =   post.post_id
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('user_post', kwargs={'pk': self.post_id})
+
 class Message_Board(ListView):
-    model = User_Post
-    template_name = 'message_board/message_board.html'
+    model               = User_Post
+    ordering            = ['-date_time']                   # Orders to most recent date
     context_object_name = 'user_posts'
-    ordering = ['-date_time']                           # Orders to most recent date
+    template_name       = 'message_board/message_board.html'
 
 class User_Post_Detail(DetailView):
     model = User_Post
+
+
+class My_Posts(ListView):
+    model               = User_Post
+    ordering            = ['-date_time']                   # Orders to most recent date
+    context_object_name = 'user_posts'
+    template_name       = 'message_board/my_posts.html'
