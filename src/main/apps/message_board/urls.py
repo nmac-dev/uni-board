1c047d9d7f3121caa68a261aea2b5e70edc5f796
ntwmac-dev <ntwmac.dev@gmail.com>
2021-03-08 01:26:30 +0000

    Added: Update Post (with validation)

--- src/main/apps/message_board/urls.py
+++ src/main/apps/message_board/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from .views import (
     Message_Board,
     Create_Post,
-    User_Post_Detail,
+    Update_Post,
+    Post_Detail,
     My_Posts
 )
 
 urlpatterns = [
-    path('',                Message_Board.as_view(),    name='message_board'    ),
-    path('create/',         Create_Post.as_view(),      name='user_create_post' ),
-    path('<int:pk>/',       User_Post_Detail.as_view(), name='user_post'        ),
-    path('my_posts/',       My_Posts.as_view(),         name='my_posts'         ),
+    path('',                    Message_Board.as_view(),    name='message_board'),
+    path('create/',             Create_Post.as_view(),      name='create_post'  ),
+    path('<int:pk>/update/',    Update_Post.as_view(),      name='update_post'  ),
+    path('<int:pk>/',           Post_Detail.as_view(),      name='post_detail' ),
+    path('my_posts/',           My_Posts.as_view(),         name='my_posts'     ),
 ]
--- src/main/apps/message_board/views.py
+++ src/main/apps/message_board/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts           import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls                import reverse
 from .models                    import User_Post
 from django.views.generic import (
@@ -14,9 +14,10 @@ from django.views.generic import (
 class Create_Post(LoginRequiredMixin, CreateView):
     model           = User_Post
     fields          = ['post_subject', 'post_content']
-    template_name   = "message_board/user_create_post.html"
+    template_name   = "message_board/create_post.html"
     post_id = None
 
+    # Validate user is logged in
     def form_valid(self, form):
         form.instance.post_user = self.request.user
         post            =   form.save()
@@ -24,7 +25,32 @@ class Create_Post(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('user_post', kwargs={'pk': self.post_id})
+        return reverse('post_detail', kwargs={'pk': self.post_id})
+
+
+class Update_Post(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model           = User_Post
+    fields          = ['post_subject', 'post_content']
+    template_name   = "message_board/update_post.html"
+    post_id = None
+
+    # Validate user is logged in
+    def form_valid(self, form):
+        form.instance.post_user = self.request.user
+        post            =   form.save()
+        self.post_id    =   post.post_id
+        return super().form_valid(form)
+
+    # Validate user owns post
+    def test_func(self):
+        isPostOwner = False
+        post_detail = self.get_object()
+        if self.request.user == post_detail.post_user:
+            isPostOwner = True
+        return isPostOwner
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.post_id})
 
 class Message_Board(ListView):
     model               = User_Post
@@ -32,9 +58,9 @@ class Message_Board(ListView):
     context_object_name = 'user_posts'
     template_name       = 'message_board/message_board.html'
 
-class User_Post_Detail(DetailView):
+class Post_Detail(DetailView):
     model = User_Post
-
+    template_name   = "message_board/post_detail.html"
 
 class My_Posts(LoginRequiredMixin, ListView):
     model               = User_Post
