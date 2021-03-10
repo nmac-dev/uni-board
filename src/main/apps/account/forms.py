858e57ae5e2974e7a9c098e60b7afd98ab75307b
Geo B <44928061+GBowz@users.noreply.github.com>
2021-03-10 17:03:49 +0000

    T&C added and Fixing styling for phones

--- src/main/apps/account/forms.py
+++ src/main/apps/account/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.safestring import mark_safe
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    terms_and_conditions = forms.BooleanField()
+    
 
     class Meta:
         model = User   #register going to save to User model
-        fields = ['username','email','password1','password2']
+        fields = ('username','email','password1','password2','terms_and_conditions')
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
--- src/main/apps/account/urls.py
+++ src/main/apps/account/urls.py
@@ -5,13 +5,14 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("",            user_views.account,                                                      name="account"  ),
-    path('register/',   user_views.register,                                                     name='register' ),
-    path('login/',      auth_views.LoginView.as_view(   template_name='account/login.html'    ), name='login'    ),
-    path('logout/',     auth_views.LogoutView.as_view(  template_name='account/logout.html'   ), name='logout'   ),
-    path('profile/',    user_views.profile,                                                      name='profile'  ),
-    path('profile/edit/', user_views.edit_profile,                                               name='edit_profile'),
-    path('change-password/', user_views.change_password,                                         name='change_password'),
+    path("",                 user_views.account,                                                      name="account"  ),
+    path('register/',        user_views.register,                                                     name='register' ),
+    path('login/',           auth_views.LoginView.as_view(   template_name='account/login.html'    ), name='login'    ),
+    path('logout/',          auth_views.LogoutView.as_view(  template_name='account/logout.html'   ), name='logout'   ),
+    path('profile/',         user_views.profile,                                                      name='profile'  ),
+    path('profile/edit/',    user_views.edit_profile,                                                 name='edit_profile'),
+    path('change-password/', user_views.change_password,                                              name='change_password'),
+    path('terms',            user_views.terms,                                                        name='terms'),
 
 ## password recovery views
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'),
