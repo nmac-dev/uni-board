6b0a7b04db888fb7054ee6c8d4a5b9f568db36e8
Will Tindall <will.tindall@live.com>
2021-03-11 10:01:53 +0000

    Add quiz tests

--- src/main/apps/quiz/tests.py
+++ src/main/apps/quiz/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from django.db import models
+from django.contrib.auth import get_user_model
+from main.apps.quiz.models import Quiz_Model
+from datetime import datetime
 
-# Create your tests here.
+class QuizModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        user = get_user_model().objects.create_user(username='test', password='testpword1', email='test@example.com')
+        user.save()
+        Quiz_Model.objects.create(
+            title='Title', 
+            desc='Description',
+            creator=user,
+            date=datetime.now(),
+            Question_1='q1',
+            Question_1_Option_1='option1')
+
+    def test_title_label(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        field_label = quiz._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_desc_label(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        field_label = quiz._meta.get_field('desc').verbose_name
+        self.assertEqual(field_label, 'desc')
+
+    def test_date_label(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        field_label = quiz._meta.get_field('date').verbose_name
+        self.assertEqual(field_label, 'date')
+
+    def test_question_label(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        field_label = quiz._meta.get_field('Question_1').verbose_name
+        self.assertEqual(field_label, 'Question 1')
+
+    def test_option_label(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        field_label = quiz._meta.get_field('Question_1_Option_1').verbose_name
+        self.assertEqual(field_label, 'Question 1 Option 1')
+
+    def test_question_max_length(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        max_length = quiz._meta.get_field('Question_1').max_length
+        self.assertEqual(max_length, 1024)
+
+    def test_option_max_length(self):
+        quiz = Quiz_Model.objects.get(quiz_id=1)
+        max_length = quiz._meta.get_field('Question_1_Option_1').max_length
+        self.assertEqual(max_length, 2048)
