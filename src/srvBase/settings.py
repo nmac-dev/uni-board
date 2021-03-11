044edc689f4ca0982547325a93e9caff129708da
kid7960 <33873145+kid7960@users.noreply.github.com>
2021-03-11 03:53:45 +0000

    Add files via upload

--- src/srvBase/settings.py
+++ src/srvBase/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'no.reply.uniboard@gmail.com'
 EMAIL_HOST_PASSWORD = 'udp8PtdyrBHxBuR'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- src/srvBase/urls.py
+++ src/srvBase/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/',          admin.site.urls                         ),
-    path('',                include('main.apps.message_board.urls'  )),
+    path("",                include("main.urls"                     )),
     path('account/',        include('main.apps.account.urls'        )),
     path('leaderboard/',    include('main.apps.leaderboard.urls'    )),
     path('message_board/',  include('main.apps.message_board.urls'  )),
     path('quiz/',           include('main.apps.quiz.urls'           )),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
